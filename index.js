20c07cb44a568691ecefaee24c860ea4c3829ac2
akshayhl2196 <109200073+akshayhl2196@users.noreply.github.com>
2022-07-13 13:54:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
